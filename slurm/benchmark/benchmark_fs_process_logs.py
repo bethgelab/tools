e543a3f7c408b1e06e814684f8be89fbeed967eb
George Pachitariu <george.pachitariu@gmail.com>
2022-05-01 19:33:41 +0200

    Improved script

--- slurm/benchmark/benchmark_fs_process_logs.py
+++ slurm/benchmark/benchmark_fs_process_logs.py
@@ -9,23 +9,24 @@ cd /home/george/git/tools/slurm
 python benchmark_fs_process_logs.py
 """
 
-
 import os
 import numpy as np
+import re
 
 lines = []
-dir="/home/george/Desktop/tuebingen_logs" # "karolina_logs"
-for file in os.listdir(dir):
-    with open(dir+"/"+file, "r") as r:
+# dir="/home/george/Desktop/tuebingen_logs" # "karolina_logs"
+dir = "/home/george/Desktop/logs/gpach_tuebingen_test"
+for folder in os.listdir(dir):
+    with open(dir+"/"+folder+"/"+"logs", "r") as r:
         for line in r.read().splitlines():
-            prefix = "/mnt/beegfs/bethge/gpachitariu37/benchmark/slurm_script"
+            prefix = "/mnt/beegfs/bethge/gpachitariu37/gpach_tuebingen_test"
             if line.startswith(prefix):
                 lines.append(line[len(prefix)+1:])
         
 dict = {}
 for l in lines:
     splits=l.split(" ")
-    key=" ".join(splits[:-1])
+    key=" ".join(splits[1:-1])
     value = splits[-1]
 
     if key not in dict:
@@ -35,12 +36,26 @@ for l in lines:
 
 s = ""
 for k, v in dict.items():
-    k = ("small files" if "small_files" in k else "large_files") + k.split("  ")[-1]
+    if "Deleting" in k:
+        continue
 
-    s += f"{k} mean:{np.round(np.mean(v), 2)} "
-    s += f"min:{np.round(np.min(v), 2)} "
-    s += f"max:{np.round(np.max(v), 2)} "
-    s += f"count:{len(v)}"
+    k = re.sub(' +', ' ', k)
+
+    d = 0
+    d = 0.14648 if "150K" in k else d
+    d = 1 if "1M" in k else d
+    d = 1024 if "1G" in k else d
+    
+    no_files=int(k.split(" ")[6])
+
+    mean = np.round(np.mean(v), 2)
+    min = np.round(np.min(v), 2)
+    max = np.round(np.max(v), 2)
+    
+    s += f"{k}\tmean (min|max):{mean} ({min} | {max})\t"
+    s += f"count:{len(v)}\t"
+    s += f"GB/s:{np.round(len(v) * no_files * d / (mean * 1024), 2)}"
+
     s += "\n"
 
 with open("log_results.txt", "w") as w:
